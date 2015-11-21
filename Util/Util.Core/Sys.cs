d06ff0b5a2f0bff578f2a9460a8f4d436e02c384
xfyrose <xfyrose@yahoo.com>
2015-11-22 00:09:19 +0800

    20151121.99.02

---
+++ Util/Util.Core/Sys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Util.Core
+{
+    public class Sys
+    {
+        public static Type GetType<T>()
+        {
+            return Nullable.GetUnderlyingType(typeof(T)) ?? typeof (T);
+        } 
+    }
+}
